74a6c340085ab6b4ab62bb87e03abd12eeeb05af
git1 <nauthyhh@outlook.com>
2022-06-16 21:55:59 +0800

    third

---
+++ demo/src/components/Exhibition.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="wrap">
+      <div class="head">
+        <div class="title">{{ title }}</div>
+        <div class="guideList">
+          <a href="##" v-for="(item, index) in guideList" :key="index">{{
+            item
+          }}</a>
+        </div>
+      </div>
+      <div class="content">
+123
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, default: "" },
+    guideList: { type: Array },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.wrap {
+  width: 1200px;
+  margin: 0 auto;
+  .head {
+    display: flex;
+    height: 48px;
+    width: 100%;
+    justify-content: space-between;
+    .title {
+      color: #c81623;
+      font-size: 20px;
+      font-weight: bold;
+    }
+    .guideList {
+      a {
+        padding: 0 10px;
+        & + a {
+          border-left: 1px solid #b3aeae;
+        }
+        &:first-child{
+            color: #c81623;
+        }
+      }
+    }
+  }
+  .content{
+    height: 360px;
+    width: 100%;
+    border-top: 2px solid #c81623;
+  }
+}
+</style>
--- demo/src/views/Index.vue
+++ demo/src/views/Index.vue
@@ -2,26 +2,29 @@
     <div>
         <header-guide></header-guide>
         <SearchCom></SearchCom>
-        <Login></Login>
+        <Exhibition title="家用电器" :guideList="guideList"></Exhibition>
+        <!-- <Login></Login> -->
         <foot-guide></foot-guide>
     </div>
 </template>
 
 <script>
 import SearchCom from "../components/SearchCom.vue"
-import Login from "./Login.vue"
+// import Login from "./Login.vue"
 import FootGuide from '../components/FootGuide.vue'
 import HeaderGuide from '../components/HeaderGuide.vue'
+import Exhibition from "../components/Exhibition.vue"
     export default {
         components:{
             HeaderGuide,
             SearchCom,
             FootGuide,
-            Login,
+            // Login,
+            Exhibition
         },
         data(){
             return{
-
+                guideList:["热卖","大家电","生活电器"," 厨房电器","应季电器","空气","净水","高端电器"]
             }
         },
         mounted(){
